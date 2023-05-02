bd7b5dd1a016fafd47bc78541b6c2c6ae17103c3
melek seba <melekseba34@gmail.com>
2023-05-02 10:07:24 -0400

    student_classe_pratice

--- src/ScienceStudent.java
+++ src/ScienceStudent.java
@@ -1,9 +1,13 @@
-public class ScienceStudent {
-
-    //COMPLETE THIS CLASS AFTER STUDENT CLASS
-    //ScienceStudent must extend to Student class - do whatever it requires
-
+public class ScienceStudent extends Student {
 
     //HERE! Create a 5 args constructor that chains super class constructor
 
+
+    public ScienceStudent(String firstName, String lastName, int age, String gender, String className)
+
+    {
+        super(firstName, lastName, age, gender, "Science");
+    }
+
+    // Add any additional methods or variables specific to ScienceStudent here
 }
